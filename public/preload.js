a0a1b4c066736389c87bff91713b338f63299be0
horizon7006 <mgthedasher@gmail.com>
2025-04-25 12:00:11 +0300

    Add files via upload

---
+++ public/preload.js
@@ -0,0 +1,1 @@
+// Optional preload script
---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+  return <div style={{ padding: 20 }}><h1>BlockShell MVP 🚀</h1></div>;
+};
+
+export default App;
